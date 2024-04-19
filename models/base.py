f4a5aefa766d6a54fe98b905aa796e497aa0ca04
Lucas Moura <lucaspenha471@gmail.com>
2024-04-18 22:20:06 -0300

    Update default datetime to utcnow() for created_at in database base class

--- models/base.py
+++ models/base.py
@@ -18,7 +18,7 @@ class SQLModel(DeclarativeBase):
 
     id: Mapped[uuid.UUID] = mapped_column('id', primary_key=True, default=uuid.uuid4)
     status: Mapped[bool] = mapped_column('status', default=True)
-    created_at: Mapped[datetime.datetime] = mapped_column('created_at', default=datetime.datetime.now())
+    created_at: Mapped[datetime.datetime] = mapped_column('created_at', default=datetime.datetime.utcnow())
     edited_at: Mapped[datetime.datetime] = mapped_column('edited_at')
     deleted_at: Mapped[datetime.datetime] = mapped_column('deleted_at')
 
