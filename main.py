f98f844173863916bb8299cae74d03fc035295c3
Lucas Moura <lucaspenha471@gmail.com>
2024-09-05 20:56:50 -0300

    Add basic config to register service in eureka server

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from backend.settings import settings
 from routers import reading, item, author, core
+import py_eureka_client.eureka_client as eureka_client
+
+# Configurações do Eureka
+EUREKA_SERVER = "http://localhost:8761/eureka"
+APP_NAME = "library-service"
+PORT = 8000
 
 app = FastAPI(
     title=settings.project_name,
@@ -10,6 +18,18 @@ app = FastAPI(
     swagger_ui_parameters={"defaultModelsExpandDepth": -1},
 )
 
+async def register_service():
+    # Iniciar o cliente Eureka e registrar o serviço
+    await eureka_client.init_async(
+        eureka_server=EUREKA_SERVER,
+        app_name=APP_NAME,
+        instance_port=PORT
+    )
+
+@app.on_event("startup")
+async def on_startup():
+    # Registrar o serviço no Eureka quando o FastAPI iniciar
+    await register_service()
 
 app.include_router(item.router)
 app.include_router(reading.router)
