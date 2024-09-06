52a66f190296ff46b86b8c908b39fabee927e407
Lucas Moura <lucaspenha471@gmail.com>
2024-09-06 12:22:15 -0300

    Add new settings for Eureka service Add Eureka discovery in initialization

--- backend/settings.py
+++ backend/settings.py
@@ -10,12 +10,15 @@ class Settings(BaseSettings):
     test: bool = False
 
     # Project description
-    project_name: str = "Library Service Microservice"
+    project_name: str = 'library-service'
+    project_title: str = "Library Service Microservice"
     project_description: str = "This service contains all necessary functions to manage a library"
     project_version: str = "0.0.1"
 
     # Secrets settings
     jwt_token_secret: str = "my_dev_secret"
 
+    # Server and Gateway
+    eureka_server: str = 'http://localhost:8761/eureka'
 
 settings = Settings()
--- main.py
+++ main.py
@@ -7,29 +7,33 @@ from routers import reading, item, author, core
 import py_eureka_client.eureka_client as eureka_client
 
 # Configurações do Eureka
-EUREKA_SERVER = "http://localhost:8761/eureka"
+EUREKA_SERVER = "http://192.168.0.29:8761/eureka"
 APP_NAME = "library-service"
-PORT = 8000
+PORT = 9001
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Registro no Eureka ao iniciar o ciclo de vida
+    await eureka_client.init_async(
+        eureka_server=EUREKA_SERVER,
+        app_name="my-fastapi-service",
+        instance_port=8001,
+        instance_host='192.168.0.29',
+    )
+    try:
+        yield
+    finally:
+        await eureka_client.stop_async()
 
 app = FastAPI(
-    title=settings.project_name,
+    title=settings.project_title,
     description=settings.project_description,
     version=settings.project_version,
     swagger_ui_parameters={"defaultModelsExpandDepth": -1},
+    lifespan=lifespan,
 )
 
-async def register_service():
-    # Iniciar o cliente Eureka e registrar o serviço
-    await eureka_client.init_async(
-        eureka_server=EUREKA_SERVER,
-        app_name=APP_NAME,
-        instance_port=PORT
-    )
 
-@app.on_event("startup")
-async def on_startup():
-    # Registrar o serviço no Eureka quando o FastAPI iniciar
-    await register_service()
 
 app.include_router(item.router)
 app.include_router(reading.router)
