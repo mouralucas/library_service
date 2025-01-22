052fcba227400d3f49e391ab19518dc535561303
Lucas Moura <lucaspenha471@gmail.com>
2025-01-22 09:59:32 -0300

    Add model config to CreateAuthorRequest schema

--- schemas/request/author.py
+++ schemas/request/author.py
@@ -1,9 +1,14 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict, AliasGenerator
+from pydantic.alias_generators import to_camel
 
 
 class CreateAuthorRequest(BaseModel):
+    model_config = ConfigDict(from_attributes=True, alias_generator=AliasGenerator(
+        alias=to_camel
+    ))
+
     name: str = Field(..., alias='authorName', description='The name of the author')
-    country_id: str = Field(None, alias='countryId', description='The country of the author')
-    language_id: str = Field(None, alias='languageId', description='The language of writing')
-    description: str = Field(None, alias='description', description='The description of the author')
-    is_translator: bool = Field(False, alias='isTranslator', description='Whether the author should be translated')
+    country_id: str = Field(None, description='The country of the author')
+    language_id: str = Field(None, description='The language of writing')
+    description: str = Field(None, description='The description of the author')
+    is_translator: bool = Field(False, description='Whether the author should be translated')
