0bf252af27e27a8de05912b7b9eae8879217d40e
Lucas Moura <lucaspenha471@gmail.com>
2025-05-08 22:10:23 -0300

    Add endpoint to access the playground in browser

--- routers_graphql/main.py
+++ routers_graphql/main.py
@@ -1,10 +1,11 @@
 from ariadne import load_schema_from_path, QueryType, MutationType, make_executable_schema, graphql_sync
 from ariadne.asgi import GraphQL
+from ariadne.explorer import ExplorerPlayground
 from fastapi import APIRouter, Request, Depends, Security
 from rolf_common.schemas.auth import RequiredUser
 from rolf_common.services import get_user
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, HTMLResponse
 
 from backend.database import get_session
 from resolvers.item import bind_item_resolvers
@@ -31,11 +32,18 @@ schema = make_executable_schema(type_defs, query, mutation)
 @router.post('/graphql')
 async def graphql_server(
         request: Request,
-        user: RequiredUser = Security(get_user),
+        # user: RequiredUser = Security(get_user),
         session: AsyncSession = Depends(get_session),
 ):
+    user = None
     data = await request.json()
     value = {"request": request, "session": session, "user": user}
     success, result = graphql_sync(schema, data, context_value=value, debug=True)
     status_code = 200 if success else 400
     return JSONResponse(result, status_code=status_code)
+
+playground_html = ExplorerPlayground().html(None)
+
+@router.get("/graphql")
+async def graphql_playground():
+    return HTMLResponse(playground_html)
