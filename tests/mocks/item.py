14953a80b6f7b887ecd72d9129ca3fc0d9f59953
Lucas Moura <lucaspenha471@gmail.com>
2025-09-01 23:34:28 -0300

    Formatted some mock files

--- tests/mocks/item.py
+++ tests/mocks/item.py
@@ -1,6 +1,5 @@
 import pytest_asyncio
 from rolf_common.managers import BaseDataManager
-from rolf_common.models import SQLModel
 
 from data_mock.item import get_item_mock, get_item_status_relation_mock
 from models import ItemModel, ItemStatusModel
@@ -8,12 +7,26 @@ from schemas.item import ItemSchema
 
 
 @pytest_asyncio.fixture
-async def create_item(create_test_session, create_languages,
-                      create_series, create_publisher,
-                      create_collections, create_authors, create_item_status) -> list[ItemSchema]:
-    data_: list[SQLModel] = await BaseDataManager(create_test_session).add_or_ignore_all(ItemModel, get_item_mock())
-    item_list: list[ItemSchema] = [ItemSchema.model_validate(data['ItemModel']) for data in data_]
+async def create_item(
+    create_test_session,
+    create_languages,
+    create_series,
+    create_publisher,
+    create_collections,
+    create_authors,
+    create_item_status,
+) -> list[ItemSchema]:
+    data_ = await BaseDataManager(create_test_session).add_or_ignore_all(
+        ItemModel, get_item_mock()
+    )
+    item_list: list[ItemSchema] = (
+        [ItemSchema.model_validate(data["ItemModel"]) for data in data_]
+        if data_
+        else []
+    )
 
-    await BaseDataManager(create_test_session).add_or_ignore_all(ItemStatusModel, get_item_status_relation_mock())
+    await BaseDataManager(create_test_session).add_or_ignore_all(
+        ItemStatusModel, get_item_status_relation_mock()
+    )
 
     return item_list
--- tests/mocks/reading.py
+++ tests/mocks/reading.py
@@ -1,39 +1,69 @@
 import pytest_asyncio
 from rolf_common.managers import BaseDataManager
 
-from data_mock.reading import get_active_reading_mock, get_reading_list_one_active_mock, get_reading_progress_mock
+from data_mock.reading import (
+    get_active_reading_mock,
+    get_reading_list_one_active_mock,
+    get_reading_progress_mock,
+)
 from models import ReadingModel, ReadingProgressModel
 from schemas.reading import ProgressSchema, ReadingSchema
 
 
 @pytest_asyncio.fixture
-async def create_more_than_one_reading(create_test_session, create_item, create_reading_status):
-    data_ = await BaseDataManager(create_test_session).add_or_ignore_all(ReadingModel, get_reading_list_one_active_mock())
-    readings = [ReadingSchema.model_validate(data['ReadingModel']) for data in data_]
+async def create_more_than_one_reading(
+    create_test_session, create_item, create_reading_status
+):
+    data_ = await BaseDataManager(create_test_session).add_or_ignore_all(
+        ReadingModel, get_reading_list_one_active_mock()
+    )
+    readings = (
+        [ReadingSchema.model_validate(data["ReadingModel"]) for data in data_]
+        if data_
+        else []
+    )
 
     return readings
 
 
 @pytest_asyncio.fixture
-async def create_active_active_readings(create_test_session, create_item, create_reading_status) -> list[ReadingSchema]:
+async def create_active_active_readings(
+    create_test_session, create_item, create_reading_status
+) -> list[ReadingSchema]:
     """
         This fixture create active readings from different items.
-        It can create any numbers of readings as it's necessary, but to maintain integrity of the business rules, each active reading must have
-         its own item. One item cannot be in more than one active reading.
+        It can create any numbers of readings as it's necessary,
+            but to maintain integrity of the business rules,
+            each active reading must have its own item.
+        One item cannot be in more than one active reading.
     :param create_test_session:
     :param create_item:
     :param create_reading_status:
     :return: the list of active readings
     """
-    data_ = await BaseDataManager(create_test_session).add_or_ignore_all(ReadingModel, get_active_reading_mock())
-    readings = [ReadingSchema.model_validate(data['ReadingModel']) for data in data_]
+    data_ = await BaseDataManager(create_test_session).add_or_ignore_all(
+        ReadingModel, get_active_reading_mock()
+    )
+    readings = (
+        [ReadingSchema.model_validate(data["ReadingModel"]) for data in data_]
+        if data_
+        else []
+    )
 
     return readings
 
 
 @pytest_asyncio.fixture
-async def create_progress(create_test_session, create_active_active_readings) -> list[ProgressSchema]:
-    data_ = await BaseDataManager(create_test_session).add_or_ignore_all(ReadingProgressModel, get_reading_progress_mock())
-    progress = [ProgressSchema.model_validate(data['ReadingProgressModel']) for data in data_]
+async def create_progress(
+    create_test_session, create_active_active_readings
+) -> list[ProgressSchema]:
+    data_ = await BaseDataManager(create_test_session).add_or_ignore_all(
+        ReadingProgressModel, get_reading_progress_mock()
+    )
+    progress = (
+        [ProgressSchema.model_validate(data["ReadingProgressModel"]) for data in data_]
+        if data_
+        else []
+    )
 
     return progress
