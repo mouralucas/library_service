c4d9bc54b6ab4c12a466bb3ec8c0c38aa8789262
Lucas Moura <lucaspenha471@gmail.com>
2025-09-16 20:46:50 -0300

    Removed unused resolver to get single item Fixed naming from create item input

--- resolvers/item.py
+++ resolvers/item.py
@@ -1,15 +1,6 @@
 from schemas.request.item import CreateItemRequest, GetItemRequest
 from services.item import ItemService
 
-
-async def resolve_get_item(_, info, id):
-    item = await ItemService(
-        session=info.context["session"], user=info.context["user"]
-    ).get_item_by_id(id)
-
-    return item.model_dump(by_alias=True)
-
-
 async def get_items_resolver(_, info, params):
     params_ = GetItemRequest.model_validate(params)
 
@@ -31,7 +22,6 @@ async def create_item_resolver(_, info, item):
 
 
 def bind_item_resolvers(query, mutation):
-    query.set_field("getItem", resolve_get_item)
     query.set_field("getItems", get_items_resolver)
 
     mutation.set_field("createItem", create_item_resolver)
